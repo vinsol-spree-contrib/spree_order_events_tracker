6279c1d6324bde8458cec13f9a902c5f4b4655a5
Himanshu <himanshumishra3@gmail.com>
2018-09-19 14:05:25 +0530

    helper method added

--- app/helpers/spree/admin/base_helper_decorator.rb
+++ app/helpers/spree/admin/base_helper_decorator.rb
@@ -6,5 +6,27 @@ Spree::Admin::BaseHelper.class_eval do
   def timeline_image(action)
     (action == 'update') ? 'cd-icon-picture.svg' : 'cd-icon-movie.svg'
   end
+
+  def timeline_container(title, date, image, &block)
+    if image =~ /location/
+      image_type = "location"
+    elsif image =~ /movie/
+      image_type = "movie"
+    else
+      image_type = "picture"
+    end
+    """
+    <div class=\"cd-timeline-block\">
+      <div class=\"cd-timeline-img cd-#{image_type}\">
+        #{image_tag image}
+      </div>
+      <div class=\"cd-timeline-content\">
+        <h2>#{title}</h2>
+        #{capture(&block)}
+        <span class=\"cd-date\">#{l(date, format: :short)}</span>
+      </div>
+    </div>
+    """.html_safe
+  end
 end
 
